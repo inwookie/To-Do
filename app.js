5b46fa565c30e845a7904f46d700b7e8b6edc62e
inwookie <31870931+inwookie@users.noreply.github.com>
2021-08-21 11:23:51 +0900

    Update Simple Calculator
    console.log -> return

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const calculator = {
   add: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
   times: function (a, b) {
-    console.log(a * b);
+    return a * b;
   },
   power: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
 };
 
-calculator.add(1, 3);
-calculator.minus(1, 3);
-calculator.divide(1, 3);
-calculator.times(1, 3);
-calculator.power(2, 4);
+console.log(calculator.add(1, 3));
+console.log(calculator.minus(1, 3));
+console.log(calculator.divide(1, 3));
+console.log(calculator.times(1, 3));
+console.log(calculator.power(2, 4));
+
+const addExample = calculator.add(21, 34);
+console.log(addExample);
