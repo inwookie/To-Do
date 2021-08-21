64467b880f2c4e459962ca00baded67604e7b0c0
inwookie <31870931+inwookie@users.noreply.github.com>
2021-08-21 11:15:54 +0900

    Update Simple Calculator

---
+++ app.js
@@ -0,0 +1,23 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  times: function (a, b) {
+    console.log(a * b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(1, 3);
+calculator.minus(1, 3);
+calculator.divide(1, 3);
+calculator.times(1, 3);
+calculator.power(2, 4);
