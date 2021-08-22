7c519e0908217e318ec95ba8681db83670d6e774
inwookie <31870931+inwookie@users.noreply.github.com>
2021-08-22 11:02:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  const givenUsername = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, givenUsername);
+  paintGreetings(givenUsername);
 }
 
 function paintGreetings(username) {
