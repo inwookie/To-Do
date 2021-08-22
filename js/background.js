2f8caace6351e1e8a3ac0796ab259bd177fc9599
inwookie <31870931+inwookie@users.noreply.github.com>
2021-08-22 18:17:32 +0900

    Update style.css

--- js/background.js
+++ js/background.js
@@ -1,9 +1,13 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"];
+// const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+// const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+document.body.style = `background-image: url("img/${Math.floor(
+  Math.random() * 5
+)}.jpeg");`;
